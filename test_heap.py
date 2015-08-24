4989bdd9b2a83d279d2200cb10bb463104c91c3b
Steven Das <steven.c.das@gmail.com>
2015-08-24 03:39:30 -0500

    separating out heap tests

--- test_heap.py
+++ test_heap.py
@@ -6,8 +6,8 @@ import unittest
 import heap
 
 
-class TestHeap(unittest.TestCase):
-  """Tests the heap data structure."""
+class TestInit(unittest.TestCase):
+  """Tests creation of a new heap."""
 
   def test_created_empty(self):
     """By default, a new heap should be created empty."""
@@ -17,3 +17,15 @@ class TestHeap(unittest.TestCase):
       h.poll()
     with self.assertRaises(ValueError):
       h.pop()
+
+
+class TestPoll(unittest.TestCase):
+  """Tests polling of heap's root element.
+
+  I.e., where by "polling," we mean examining the value of the heap's root
+  element WITHOUT removing it or otherwise altering the heap's state in any way.
+  """
+  pass
+
+
+class TestPop(unittest.TestCase):
