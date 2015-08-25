c6de76e2a2b3f63ee4288c8c8cb33dfa2f78d1ca
Steven Das <steven.c.das@gmail.com>
2015-08-25 00:23:43 -0500

    update tests

--- test_bst.py
+++ test_bst.py
@@ -12,7 +12,20 @@ class TestInit(unittest.TestCase):
   def test_empty_by_default(self):
     """By default, a new binary search tree should be created empty."""
     tree = bst.BinarySearchTree()
+    with self.assertRaises(ValueError):
+      tree.root
     self.assertEqual(tree.size, 0)
     self.assertEqual(tree.preorder, [])
     self.assertEqual(tree.inorder, [])
     self.assertEqual(tree.postorder, [])
+
+
+class TestInsert(unittest.TestCase):
+  """Tests insertion into a binary search tree."""
+
+  def test_insert_into_empty_tree(self):
+    """Anything inserted into an empty BST should become root."""
+    tree = bst.BinarySearchTree()
+    tree.insert(42)
+    self.assertEqual(tree.size, 1)
+    self.assertEqual(tree.root, 42)
