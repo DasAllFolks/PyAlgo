ef7ce7b31a09b8452bfb134e4ce04022d675f0d0
Steven Das <steven.c.das@gmail.com>
2015-08-24 23:52:29 -0500

    binary search tree file

---
+++ bst.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class BinarySearchTree(object):
+  """Implements a binary search tree in Python."""
+
+  class _Node(object):
+    """Represents a node in the binary search tree."""
+
+    value = None
+    left = None
+    right = None
