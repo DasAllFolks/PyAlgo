d3b8afd6a2d29ec1cb6977239d1c666e6f4027dc
Steven Das <steven.c.das@gmail.com>
2015-08-25 00:12:16 -0500

    Initial unit tests for basic BST

---
+++ test_bst.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+
+import bst
+
+
+class TestInit(unittest.TestCase):
+  """Tests creation of a new binary search tree."""
+
+  def test_empty_by_default(self):
+    """By default, a new binary search tree should be created empty."""
+    tree = bst.binarySearchTree()
+    self.assertEqual(tree.size, 0)
+    self.assertEqual(tree.preorder, [])
+    self.assertEqual(tree.inorder, [])
+    self.assertEqual(tree.postorder, [])
